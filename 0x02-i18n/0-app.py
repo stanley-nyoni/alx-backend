32e75771f2089ece0243892805c3779b688bc494
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-08-27 16:17:01 +0200

    start flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+
+"""0. Basic Flask app
+Start a basic Flask web application
+"""
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Display a welcome message"""
     return render_template("0-index.html", title="Welcome to Holberton")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
